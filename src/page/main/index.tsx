c96ed40a5d5b03df5d41e2de54b5e70d986c3871
jochongs <joch2712@naver.com>
2025-01-07 16:07:34 +0900

    chore: Apply font, Update tailwind config

---
+++ src/page/main/index.tsx
@@ -0,0 +1,13 @@
+export const MainPage = () => {
+  return (
+    <div>
+      <p className="font-pretendard font-thin">다람쥐 헛 챗바퀴 뭐 어쩌구</p>
+      <p className="font-pretendard font-extralight">
+        다람쥐 헛 챗바퀴 뭐 어쩌구
+      </p>
+      <p className="font-pretendard font-bold">다람쥐 헛 챗바퀴 뭐 어쩌구</p>
+      <p className="border-[1px] border-solid">안녕</p>
+      <div className="bg-black">테스트</div>
+    </div>
+  );
+};
