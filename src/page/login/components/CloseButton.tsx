e234d511f20f97f3a4b456f7d6abb25f976a2b02
jochongs <joch2712@naver.com>
2025-01-08 21:15:25 +0900

    feat: Implement login page close button event

--- src/page/login/components/CloseButton.tsx
+++ src/page/login/components/CloseButton.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import CloseIcon from '../icon/close-icon.svg';
 
 export const CloseButton = () => {
+  const router = useRouter();
+
+  const btnClickEvent = () => {
+    router.back();
+  };
+
   return (
-    <button className="w-full h-full">
+    <button className="w-full h-full" onClick={btnClickEvent}>
       <CloseIcon />
     </button>
   );
