c4c507cb37517a7485d842e319a882ad0110eeed
jochongs <joch2712@naver.com>
2025-01-10 18:50:12 +0900

    fix: Fix key error in MenuTabContent component

--- src/page/place-detail/components/MenuTabContent/index.tsx
+++ src/page/place-detail/components/MenuTabContent/index.tsx
@@ -6,8 +6,8 @@ export const MenuTabContent = () => {
       <div className="my-[24px] mx-[20px] flex flex-col gap-[16px]">
         {Array(10)
           .fill(0)
-          .map(() => (
-            <MenuCard />
+          .map((data, i) => (
+            <MenuCard key={i} />
           ))}
       </div>
     </>
